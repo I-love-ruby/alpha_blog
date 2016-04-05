bf00e469ac2bfeb436ebf06d60589a0d47b5a82e
Dongchul's  Rails Git Test <no1_ldc@hotmail.com>
2016-04-05 16:38:58 -0400

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:edit,:update,:show,:destroy]
 
   def index
     @articles = Article.all
-
   end
 
   def new
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice]="article was successfully updated"
       redirect_to article_path(@article)
@@ -34,11 +34,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = " Articles was successfully deleted"
     redirect_to articles_path
@@ -47,8 +47,14 @@ class ArticlesController < ApplicationController
 
   private
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+
 
 end
